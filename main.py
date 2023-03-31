032a1db08e6f0acba0e14afdcf59d7820bc6360f
Naman Pant <64011592+namanp247@users.noreply.github.com>
2023-03-31 13:31:49 +0530

    Changes

--- main.py
+++ main.py
@@ -1,82 +1,156 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 import os
 import sys
 from datetime import timedelta, date
-
+import re
 
 def userInfo():
-    myemail = ""
-    mypassword = ""
+    myEmail = "pant.naman2001@gmail.com"
+    myPassword = ".xvGvQ#D5vhW!kZ"
 
-    email = driver.find_element_by_xpath('//*[@id="emailaddress"]')
-    email.send_keys(myemail)
 
-    passwrd = driver.find_element_by_xpath('//*[@id="password"]')
-    passwrd.send_keys(mypassword)
+    username = driver.find_element(By.ID,"handleOrEmail")
+    username.send_keys(myEmail)
 
+    password = driver.find_element(By.ID, "password")
+    password.send_keys(myPassword)
+
+    # driver.find_element(By.XPATH,'//*[@id="remember"]').click()
     time.sleep(2)
 
     login()
 
 def login():
     try:
-        loginButton = driver.find_element_by_xpath('//*[@id="loginButton"]')
+        loginButton = driver.find_element(By.XPATH,'//*[@id="enterForm"]/table/tbody/tr[4]/td/div[1]/input')
         driver.execute_script("arguments[0].click();", loginButton)
-    except:
-        print('unable to log in')
+    except Exception as err:
+        with open('logs.txt', 'a') as logFile:
+            logFile.write('Unable to log in, invalid credentials\n')
 
-    time.sleep(2)
+    print("Logged in")
 
-    chooseDay()
+    time.sleep(5)
+    userDivs = userDiv()
+    chooseContests(userDivs) 
 
-def chooseDay():
+def userDiv():
     try:
-        selectDayButton = driver.find_element_by_xpath('//*[@id="btn_date_select"]')
-        driver.execute_script("arguments[0].click();", selectDayButton)
-    except:
-        print('unable to select day')
-        driver.quit()
-        driver.close()
+        userRating = driver.find_element(By.XPATH,'//span[@class="user-green"]')
+        userRating = int(userRating.text)
+        userDiv = 0
+        if(userRating < 1400):
+            userDiv = 4
+        elif(userRating < 1600):
+            userDiv = 3
+        elif(userRating < 1900):
+            userDiv = 2
+        else:
+            userDiv = 1
+        # print(f"User divison calculated {userDiv}")
+        return userDiv
+    except Exception as err:
+        with open('logs.txt', 'a') as log_file:
+            log_file.writelines("Unable to fetch rating\n")
+        raise err
 
-    try:
-        dateSelected = date.today() + timedelta(days=2)
-        dayButton = driver.find_element_by_xpath('//*[@id="date_' + str(dateSelected) + '"]')
-        driver.execute_script("arguments[0].click();", dayButton)
-    except:
-        print('date not there')
-        driver.quit()
-        driver.close()
+def chooseContests(userDiv):
     
-    selectTime()
-
-def selectTime():
+    # Go to contest page
     try:
-        bookTimeSlots = driver.find_element_by_xpath("//*[contains(text(), 'at 4:00 PM')]")
-        driver.execute_script("arguments[0].click();", bookTimeSlots)
-    except:
-        driver.quit()
-        driver.close()
+        contestButton = driver.find_element(By.XPATH, '//a[@href="/contests"]')
+        contestButton.click()
+        time.sleep(5)
+    except Exception as err:
+        with open('logs.txt', 'a') as logFile:
+            logFile.write('Unable to access contest page\n')
+            print(str(err))
 
-    confirmBooking()
-
-def confirmBooking():
+     # Access the Contest Table element
     try:
-        bookyesButton = driver.find_element_by_xpath('//*[@id="dialog_book_yes"]')
-        driver.execute_script("arguments[0].click();", bookyesButton)
-    except:
-        driver.quit()
-        driver.close()
+        contestTable = driver.find_element(By.XPATH, '//*[@id="pageContent"]/div[1]/div[1]')
+        contestList = contestTable.find_elements(By.TAG_NAME, "tr")[1:]
+
+        global validContestCount
+        global registeredContestCount
+        validContestCount = 0
+        registeredContestCount = 0
+
+        # Iterating every row of the table
+        for row in contestList:
+            try:
+                contestName = row.find_elements(By.TAG_NAME, "td")
+                contestName = contestName[0].text
+
+                # regex to find Contest division
+                contestDiv = [int (x) for x in re.findall(r"\. (.{1})", contestName)]
+                # print(contestName, " ", contestDiv, " ", userDiv)
+                # for x in re.findall(r"\. (.{1})", contestName):
+                #     print(x)
+                if(len(contestDiv) == 0):
+                    contestDiv = 5
+                else:
+                    contestDiv = min(contestDiv)
+
+                if (contestDiv <= userDiv):
+                    registerLink = row.find_element(By.XPATH, '//a[@class="red-link"]')
+                    validContestCount += 1
+                    registeredContestCount += registerLink.click()
+                    time.sleep(5)
+                    register()
+            except Exception as err:
+                with open('logs.txt', 'a') as log_file:
+                    log_file.writelines('Not eligible to register or Registration not opened yet\n')
+                continue
+
+        # All valid registration completed
+        if (validContestCount == registeredContestCount):
+            print("Done")
+        else:
+            print("Incomplete")
+
+    except Exception as err:
+        with open('logs.txt', 'a') as log_file:
+            log_file.writelines('Table not accesible\n')
+    time.sleep(5)
+
+def register():
+    try:
+        registerButton = driver.find_element(By.CLASS_NAME, 'submit')
+        registerButton.click()
+        return 1
+    except :
+        with open('logs.txt', 'a') as log_file:
+            log_file.writelines('Not able to register')
+    time.sleep(5)
+    return 0
 
 if __name__ == '__main__':
-    # Load chrome
-    driver = webdriver.Chrome()
-    driver.get("https://myfit4less.gymmanager.com/portal/login.asp")
+    try:
+        # Load chrome
+        driver = webdriver.Chrome()
+        driver.get("https://codeforces.com/")
+    
+        enterButton = driver.find_element(By.XPATH, '//*[@id="header"]/div[2]/div[2]/a[1]')
 
-    time.sleep(2)
-
-    userInfo()
-    driver.quit()
+        # Enter login page if user is not logged in
+        if enterButton.text == 'Enter':
+            driver.execute_script("arguments[0].click();", enterButton)
+            time.sleep(5)
+            userInfo()
+        else:
+            with open('logs.txt', 'a') as log_file:
+                log_file.write(f"User {enterButton.text} is already logged in.\n")
+            chooseContests()
+    except Exception as err:
+        with open('logs.txt', 'a') as log_file:
+            log_file.write('Site not working. Try later\n')
+        raise err
+        
+    # driver.quit()
     driver.close()
+    driver.quit()
 
